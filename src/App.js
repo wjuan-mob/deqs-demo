6ea6eb3b0545190356a2302a80e09a041ace2bfb
William J <william@mobilecoin.com>
2023-03-27 15:55:40 -0400

    stash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+	import logo from './logo.svg';
+	import './App.css';
+	
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const { DeqsClientAPIClient } = require('./deqs_grpc_web_pb.js');
+	const { SubmitQuotesRequest, SubmitQuotesResponse } = require('./deqs_pb.js');
+  const { PingPongClient } = require('./deqs_grpc_web_pb');
+	const { PingRequest, PongResponse } = require('./deqs_pb.js');
 
-export default App;
+	// var client = new DeqsClientAPIClient('http://localhost:9090', null, null);
+	var client = new PingPongClient('http://localhost:9090', null, null);
+
+	class App extends Component {
+	  
+	  callGrpcService = () => {
+	    const request = new PingRequest();
+	    request.setPing('Ping');
+	
+
+	    client.Ping(request, {}, (err, response) => {
+	      if (response == null) {
+	        console.log(err)
+	      }else {
+	        console.log(response.getPong())
+	      }
+	    });
+    }
+	
+
+	  render() {
+	    return (
+	      <div className="App">
+	        <header className="App-header">
+	          <img src={logo} className="App-logo" alt="logo" />
+	          <button style={{padding:10}} onClick={this.callGrpcService}>Click for grpc request</button>
+	        </header>
+	      </div>
+	    );
+	  }
+	}
+	
+
+	export default App;
