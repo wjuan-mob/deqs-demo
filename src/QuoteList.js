bc1478d658d2181aa11aa3d33c6f6b9ff6ca7fbc
William J <william@mobilecoin.com>
2023-03-31 05:20:19 -0400

    Add functions for translating quotes into buckets

--- src/QuoteList.js
+++ src/QuoteList.js
@@ -1,7 +1,7 @@
-import React, { Component, useState } from 'react';
-import logo from './logo.svg';
+import React, { Component, useState, useEffect, useRef } from 'react';
+import logo from './logo.png';
 import currency_config from './currency_config.json';
-// import './App.css';
+import { Tooltip } from 'react-tooltip';
 
 const { DeqsClientAPIClient } = require('./deqs_grpc_web_pb.js');
 const { Pair, GetQuotesRequest, GetQuotesResponse, Quote } = require('./deqs_pb.js');
@@ -14,6 +14,8 @@ const enableDevTools = window.__GRPCWEB_DEVTOOLS__ || (() => {
 
 var client = new DeqsClientAPIClient('http://localhost:9090', null, null);
 
+const currencyPriority = ["GBP", "EUR", "USD"];
+
 const quoteIdStyle = {
     margin: '10px 0',
     padding: '10px',
@@ -24,14 +26,253 @@ const quoteItemStyle = {
     fontWeight: 'bold'
 }
 
+
+const boxesWithScrollbarStyle = {
+    border: '1px solid black',
+    padding: '10px',
+    margin: '10px',
+    borderRadius: '10px',
+    overflow: 'hidden', // hide overflowing text
+    textOverflow: 'ellipsis', // add ellipsis to truncated text
+    whiteSpace: 'nowrap', // prevent text from wrapping
+    maxWidth: '100%', // set a maximum width to avoid expanding beyond the box
+};
+
+const truncatedStyle = {
+    border: '1px solid black',
+    padding: '10px',
+    margin: '10px',
+    borderRadius: '10px',
+    overflow: 'hidden', // hide overflowing text
+    textOverflow: 'ellipsis', // add ellipsis to truncated text
+    whiteSpace: 'nowrap', // prevent text from wrapping
+    maxWidth: '100%', // set a maximum width to avoid expanding beyond the box
+};
+
+
+
+const groupHeaderStyle = {
+    marginBottom: '10px',
+};
+
+const getPairKey = (pair) => {
+    const { base_currency, counter_currency } = pair;
+    const orderedCurrencies = [
+        base_currency,
+        counter_currency
+    ].sort((a, b) => {
+        return currencyPriority.indexOf(b) - currencyPriority.indexOf(a);
+    });
+    return `${orderedCurrencies[0]}/${orderedCurrencies[1]}`;
+};
+
+const groupQuotesByQuantityAndPair = (quotes) => {
+    return quotes.reduce((result, quote) => {
+        const quantity = quote.quantity;
+        const pair = quote.pairs[0];
+        const pairKey = getPairKey(pair);
+        result[quantity] = result[quantity] || {};
+        result[quantity][pairKey] = result[quantity][pairKey] || [];
+        result[quantity][pairKey].push(quote);
+        return result;
+    }, {});
+};
+
+const sortQuotesByBaseCurrency = (quotes, priority) => {
+    const priorityMap = priority.reduce((map, currency, index) => {
+        map[currency] = index;
+        return map;
+    }, {});
+
+    return quotes.sort((quote1, quote2) => {
+        const base1 = quote1.pairs[0].base_currency;
+        const base2 = quote2.pairs[0].base_currency;
+        const basePriority1 = priorityMap[base1] || Infinity;
+        const basePriority2 = priorityMap[base2] || Infinity;
+
+        if (basePriority1 < basePriority2) {
+            return -1;
+        } else if (basePriority1 > basePriority2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+};
+
+
+
+const combineQuotesIntoBuckets = (values, quotes) => {
+    // Group quotes by quantity and pair
+    const quantityQuotes = groupQuotesByQuantityAndPair(quotes);
+
+    // Combine quotes for each quantity and pair
+    const combinedQuotes = Object.keys(quantityQuotes).map((quantity) => {
+        const pairQuotes = quantityQuotes[quantity];
+        const pair = pairQuotes[0].pairs[0];
+        const pairKey = getPairKey(pair);
+        const sortedPairQuotes = sortQuotesByBaseCurrency(pairQuotes, currencyPriority);
+        const bid = sortedPairQuotes[0].price;
+        const ask = sortedPairQuotes[1].price;
+        return {
+            quantity,
+            pair_key: pairKey,
+            bid,
+            ask,
+        };
+    });
+
+    // Filter out combined quotes with invalid bid/ask
+    const filteredQuotes = combinedQuotes.filter(({ bid, ask }) => {
+        return bid > 0 && ask > 0 && bid < ask;
+    });
+
+    // Sort combined quotes by value and quantity
+    const sortedQuotes = sortQuotesByQuantity(filteredQuotes);
+
+    return sortedQuotes;
+};
+
+const sortQuotesByQuantity = (quotes) => {
+    return quotes.sort((quote1, quote2) => {
+      if (quote1.quantity < quote2.quantity) {
+        return -1;
+      } else if (quote1.quantity > quote2.quantity) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  };
+  
+
+
+const groupQuotesByPair = (items) => {
+    const pairsMap = {};
+
+    // Loop through each quote and group it by its pair elements
+    items.forEach((quote) => {
+        const pair = JSON.stringify(quote.pairs.sort());
+        pairsMap[pair] = pairsMap[pair] || [];
+        pairsMap[pair].push(quote);
+    });
+
+    // Convert the map to an array of grouped quotes
+    const groupedQuotes = Object.values(pairsMap);
+
+    return groupedQuotes;
+};
+
+
+const groupBy = (items, key) => {
+    return items.reduce((result, item) => {
+        const group = item[key];
+        result[group] = result[group] || [];
+        result[group].push(item);
+        return result;
+    }, {});
+};
+const groupBoxStyle = {
+    border: '1px solid black',
+    borderRadius: '10px',
+    padding: '10px',
+    marginBottom: '10px',
+    height: '300px', // set a fixed height for the grouping box
+};
+
+const groupWrapperStyle = {
+    width: '100%',
+    margin: '0',
+};
+
+const smallTextStyle = {
+    fontSize: '0.8em',
+};
+
+const renderItemName = (name) => {
+    const MAX_LENGTH = 5;
+    const displayName = name.length > MAX_LENGTH ? `${name.slice(0, MAX_LENGTH)}...` : name;
+    return (
+        <div>
+            <Tooltip id={`tooltip-${name}`} place="bottom" effect="solid">
+                {name}
+            </Tooltip>
+            <div data-tip data-for={`tooltip-${name}`}>
+                {displayName}
+            </div>
+        </div>
+    );
+};
+
+export function ItemList({ items }) {
+    const groupedItems = groupBy(items, 'pair');
+
+    return (
+        <div className="item-list" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: '10px' }}>
+            <div
+                style={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+                    gridGap: '10px',
+                }}
+            >
+                {Object.keys(groupedItems).map((group) => (
+                    <div key={group} className="item-group" style={{ backgroundColor: 'white', borderRadius: '5px', padding: '10px' }}>
+                        <h2>{group}</h2>
+                        {groupedItems[group].map((item, index) => (
+                            <div
+                                key={item.id}
+                                style={{
+                                    ...truncatedStyle,
+                                    ...(index !== 0 && smallTextStyle),
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
+                                    padding: '10px',
+                                    backgroundColor: 'transparent'
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        backgroundColor: 'rgba(120, 220, 140, 0.8)',
+                                        padding: '10px',
+                                        borderRadius: '5px',
+                                    }}
+                                >
+                                    <h3>Bid</h3>
+                                    <div>{renderItemName(item.bid)}</div>
+                                </div>
+                                <div style={{ fontSize: '24px', margin: '0 10px' }}>{item.quantity}</div>
+                                <div
+                                    style={{
+                                        backgroundColor: 'rgba(220, 120, 140, 0.8)',
+                                        padding: '10px',
+                                        borderRadius: '5px',
+                                    }}
+                                >
+                                    <h3>Ask</h3>
+                                    <div>{renderItemName(item.ask)}</div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+
 function QuoteList() {
     const [quotes, setQuotes] = useState([]);
+    const [intervalId, setIntervalId] = useState(null);
+    const countRef = useRef(0);
 
     const handleGetQuotes = () => {
         const limit = 100;
         const requests = [];
         const quotesData = [];
-
+        console.log("Count" + countRef.current);
         currency_config.pairs.forEach((config_pair) => {
             const request = new GetQuotesRequest();
             const pair = new Pair();
@@ -84,17 +325,41 @@ function QuoteList() {
                 });
 
                 setQuotes(quotesData);
+                countRef.current++; // Increment the count
             });
         });
     };
 
+    // Call handleGetQuotes() every 30 seconds
+    const startPolling = () => {
+        const id = setInterval(() => {
+            handleGetQuotes();
+        }, 30000);
+        setIntervalId(id);
+    };
+
+    const stopPolling = () => {
+        clearInterval(intervalId);
+        setIntervalId(null);
+    };
+
+    useEffect(() => {
+        startPolling();
+
+        return () => {
+            if (intervalId) {
+                clearInterval(intervalId);
+            }
+        };
+    }, []);
 
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <img src={logo} className="App-logo" alt="logo" style={{ width: '100px', height: '100px' }} />
                 <div>
                     <button onClick={handleGetQuotes}>Get Quotes</button>
+                    <button onClick={stopPolling}>Stop Polling</button>
                     <ul>
                         {quotes.map((quote) => (
                             <li key={quote.id.toString()} style={quoteIdStyle}>
